ad2417d71aeb019f888f8946dc92630252261b45
PhilRfr <phil@philr.fr>
2022-09-10 04:36:41 +0200

    line.gd : typo fix + performance increase
    Fixed the typo in "resolution" and added a simple boolean check to avoid recreating countless polygons at each frame, since a polygon only need to be done when resolution/radius is changed.

--- Line.gd
+++ Line.gd
@@ -2,13 +2,21 @@ extends Spatial
 tool
 
 export var line_radius = 0.1
-export var line_resoultion = 180
+export var line_resolution = 180
+
+onready var old_radius = line_radius
+onready var old_resolution = line_resolution
 
 func _process(delta):
+	if old_radius == line_radius and old_resolution == line_resolution:
+		return
+	print("Recomputing")
 	var circle = PoolVector2Array()
-	for degree in line_resoultion:
-		var x = line_radius * sin(PI * 2 * degree / line_resoultion)
-		var y = line_radius * cos(PI * 2 * degree / line_resoultion)
+	for degree in line_resolution:
+		var x = line_radius * sin(PI * 2 * degree / line_resolution)
+		var y = line_radius * cos(PI * 2 * degree / line_resolution)
 		var coords = Vector2(x,y)
 		circle.append(coords)
 	$CSGPolygon.polygon = circle
+	old_radius = line_radius
+	old_resolution = line_resolution
